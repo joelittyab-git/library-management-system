aaef6534a0b4ce7d563bc3a20a56764db71138c0
joelittyab-git <joeittab@gmail.com>
2024-03-05 02:46:46 +0530

    config refractor

--- manage.py
+++ manage.py
@@ -1,8 +1,14 @@
 from Management.library import LibraryManager
 from Interface.console import ConsoleRunner
-
+from Entity.admin import Admin
 
 # object instantiation
-application = ConsoleRunner(LibraryManager())
+manager:LibraryManager = LibraryManager()
+application:ConsoleRunner = ConsoleRunner(manager)
+
+#default admin
+admin = Admin("admin", "Admin@123")
+application.library.register_admin(admin)
+
 application.start()
 
