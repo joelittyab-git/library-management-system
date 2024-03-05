2f4a9d03a0a4e535a9b0574298fb2aeaef0f3e74
joelittyab-git <joeittab@gmail.com>
2024-03-05 10:27:38 +0530

    docs

--- Entity/admin.py
+++ Entity/admin.py
@@ -1,4 +1,9 @@
 class Admin(object):
+     
+     '''A class that represents an admin in a library
+     :<str>:username -> stores the username of the admin
+     :<str>:password -> stores password of the admin
+     '''
      def __init__(self,username:str, password:str) -> None:
           self.username = username
           self.password = password
